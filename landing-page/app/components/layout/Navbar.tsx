45394cde0952b395503766f11aba5bb8f7fe066a
therabbi.tim@gmail.com <you@example.com>
2026-02-23 00:55:56 +0100

    added icons for navbar again

--- landing-page/app/components/layout/Navbar.tsx
+++ landing-page/app/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import { Bell, Settings, Search } from "lucide-react"
+import { Bell, Settings, Search, User } from "lucide-react"
 
 export default function Navbar () {
   return (
@@ -13,18 +13,20 @@ export default function Navbar () {
             </h2>
         </div>
         <div className='flex items-center gap-4'>
-            <Search className='absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400'
-            size={16}
-            /> 
+            {/* <Search className="text-gray-500 cursor-pointer hover:text-gray-800" size={18}/> */}
             <input 
                 type="text" 
                 placeholder='Type here...'
                 className='hidden md:block h-8 placeholder-gray-200 bg-white border border-gray-200 rounded-lg px-4 py-2 
                             text-sm text-black focus:outline-none focus:ring-2 focus:ring-teal-500'
             />
-            <button className='text-sm text-gray-600 cursor-pointer hover:text-gray-900'>
-                Sign in
-            </button>
+            <div className="flex items-center gap-1 mb-1">
+                <User className="text-gray-500 cursor-pointer hover:text-gray-800" size={18}/>
+                <button className='text-sm text-gray-600 cursor-pointer hover:text-gray-900'>
+                    Sign in
+                </button>
+            </div>
+
             <Bell className="text-gray-500 cursor-pointer hover:text-gray-800" size={18}/>
             <Settings className="text-gray-500 cursor-pointer hover:text-gray-800" size={18}/>
         </div>  
--- landing-page/app/components/layout/Sidebar.tsx
+++ landing-page/app/components/layout/Sidebar.tsx
@@ -1,16 +1,35 @@
+import { LayoutDashboard, Table, CreditCard, Globe, Leaf } from "lucide-react"
+
 
 export default function Home() {
   return (
      
       <aside className="w-64 bg-white shadow-sm p-6 hidden md:flex md:flex-col">
-        <h1 className="text-xl font-bold mb-8 text-teal-600">
+        <div className="flex items-center gap-1 mb-8">
+        <Leaf size={32} className="text-teal-500 mb-4" />
+        <h1 className="text-xl font-bold mb-5 text-teal-600">
           Purity UI
         </h1>
+
+        </div>
         <nav className="space-y-2">
-          <SidebarItem label="Dashboard" active/>
-          <SidebarItem label="Tables"/>
-          <SidebarItem label="Billing"/>
-          <SidebarItem label="RTL"/>
+          <SidebarItem
+            label="Dashboard"
+            icon={LayoutDashboard}
+            active
+           />
+          <SidebarItem
+            label="Tables"
+            icon={Table} 
+           />
+          <SidebarItem
+            label="Billing"
+            icon={CreditCard} 
+           />
+          <SidebarItem
+            label="RTL"
+            icon={Globe} 
+           /> 
         </nav>
         
         <div className="flex-1">
@@ -31,16 +50,19 @@ export default function Home() {
 
 function SidebarItem({
   label,
+  icon: Icon,
   active = false,
 }: {
-  label: string;
+  label: string
+  icon: any
   active?: boolean;
 }){
   return (
-    <div className={`px-4 py-2 rounded-lg text-sm font-medium cursor-pointer transition ${
+    <div className={`flex items-center gap-3 px-4 py-2 rounded-lg text-sm font-medium cursor-pointer transition ${
       active ? 'bg-teal-500 text-white' : 'text-gray-600 hover:bg-gray-100'
     }`} >
-      {label}
+    <Icon size={16} />
+    {label}
 
     </div>
   )
