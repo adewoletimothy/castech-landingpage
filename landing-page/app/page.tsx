4b4014cd93bcd38494bfa9d94af3a64cafc97ad7
therabbi.tim@gmail.com <you@example.com>
2026-02-23 05:01:26 +0100

    Purity UI dashboard

--- landing-page/app/page.tsx
+++ landing-page/app/page.tsx
@@ -15,7 +15,24 @@ export default function Home() {
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
           <div className="bg-white p-4 rounded-lg shadow-sm">
-            Left Card
+            <div>
+              <p className="text-sm text-gray-500">
+                Built by developers   
+              </p>
+
+              <h1 className="text-xl font-bold text-gray-600 mt-2 leading-relaxed">
+                Purity UI Dashboard
+              </h1>
+
+              <p className="text-sm text-gray-600 mt-4 ">
+                From colors, cards, typography to complex elements, you will find the full documentation. 
+              </p>
+
+              <button className="text-sm font-medium text-teal-600  mt-6  cursor-pointer">
+                Read more →
+              </button>
+
+            </div>
           </div>
           <div className="bg-linear-to-r from-teal-500 to-teal-400 text-white rounded-xl p-6 shadow-sm">
             Middle Card
